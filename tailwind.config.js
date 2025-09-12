e74b094260cfd1de3159f8a507c3a97b420384d5
muhammad-hammad-shah <hammad4455shah5544@gmail.com>
2025-09-13 02:19:16 +0500

    changed the font to (manrope) successfully

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-manrope)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
